6eda22fb644ca57c83d68f7898719ffe36d1615c
sly <shanglingyue1992@163.com>
2016-03-28 22:26:26 +0800

    blog

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,7 @@ $(function(){
 		$(this).addClass("current").parent("li").siblings().children("a").removeClass("current");
 		var href = $(this).attr("href")+" #container";
 		$(".container").load(href);
+		var url = window.location.host+$(this).attr("href");
+		window.history.pushState(state, document.title, url);
 	});
 });
