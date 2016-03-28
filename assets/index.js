2630eb77edbc990279bf2531119878f717f1788d
sly <shanglingyue1992@163.com>
2016-03-28 22:31:43 +0800

    pushState

--- assets/index.js
+++ assets/index.js
@@ -2,9 +2,9 @@ $(function(){
 	$(".header_container ul li a").click(function(e){
 		e.preventDefault();
 		$(this).addClass("current").parent("li").siblings().children("a").removeClass("current");
-		var href = $(this).attr("href")+" #container";
+		var url =  $(this).attr("href");
+		var href = url+" #container";
 		$(".container").load(href);
-		var url = window.location.host+$(this).attr("href");
 		window.history.pushState({}, document.title, url);
 	});
 });
