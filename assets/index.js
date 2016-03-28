f511e122d94f5c7b91e3221601ef86a339e4585c
sly <shanglingyue1992@163.com>
2016-03-28 21:31:17 +0800

    nav

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ $(function(){
 	$(".header_container ul li a").click(function(e){
 		e.preventDefault();
 		$(this).addClass("current").parent("li").siblings().children("a").removeClass("current");
-		var href = $(this).attr("href")+" #container";
+		var href = $(this).attr("href")+" #content";
 		$(".container").load(href);
 	});
 });
