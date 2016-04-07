ac621ded9ff597cfcd731b2137267db98c6e66c6
shly <1028442429@qq.com>
2016-04-07 14:58:57 +0800

    task_18

---
+++ IFE/task_18/index.js
@@ -0,0 +1,55 @@
+var EventUtil = {
+  addEvent:function(ele,event,func){
+    if(ele.addEventListener){
+      ele.addEventListener(event,func,false);
+    }else if(ele.attachEvent){
+      ele.attachEvent("on"+event,fun);
+    }else{
+      ele["on"+event] = func;
+    }
+  }
+};
+EventUtil.addEvent(document.querySelector(".leftIn"),"click",leftIn);
+EventUtil.addEvent(document.querySelector(".rightIn"),"click",rightIn);
+EventUtil.addEvent(document.querySelector(".leftOut"),"click",leftOut);
+EventUtil.addEvent(document.querySelector(".rightOut"),"click",rightOut);
+EventUtil.addEvent(document.querySelector(".showBox"),"click",removeEle);
+function leftIn(){
+	var value = document.querySelector(".datavalue").value;
+	var newNode = document.createElement("div");
+	newNode.className = "item";
+	newNode.innerHTML = value;
+	document.querySelector(".showBox").insertBefore(newNode,document.querySelector(".showBox").firstChild);
+}
+function rightIn(){
+	var value = document.querySelector(".datavalue").value;
+	var newNode = document.createElement("div");
+	newNode.classNa类型me = "item";	
+	newNode.innerHTML = value;
+	document.querySelector(".showBox").appendChild(newNode);
+}
+function leftOut(){
+	var showBox = document.querySelector(".showBox");
+	if(showBox.firstChild==null){
+        alert("对不起，当前队列为空");
+	}else{
+	    var ele = showBox.removeChild(showBox.firstChild);
+	    alert(ele.innerHTML);
+	}
+}
+function rightOut(){
+	var showBox = document.querySelector(".showBox");
+	if(showBox.lastChild==null){
+        alert("对不起，当前队列为空");
+	}else{
+		var ele = showBox.removeChild(showBox.lastChild);
+		alert(ele.innerHTML);
+	}
+}
+function removeEle(event){
+	if(event.target&&event.target.className=="item"){
+		var ele = document.querySelector(".showBox").removeChild(event.target);
+		alert(ele.innerHTML);
+	}
+
+}
