fbaa2a3efcdaa44cffdb0640c8814d34de385dc2
shly <1028442429@qq.com>
2016-04-11 09:23:28 +0800

    add css to blog index and modify pushState

--- assets/index.js
+++ assets/index.js
@@ -1,21 +1,24 @@
 $(function(){
 	if(location.pathname.length==1){
-		history.replaceState({url:"/IFE/index.html"},"","IFE/index.html");
+		history.replaceState({url:"/IFE/index.html",subfolder:"blog"},"","IFE/index.html");
 	}
 	$(".header_container ul li a").click(function(e){
 		e.preventDefault();
-		$(this).addClass("current").parent("li").siblings().children("a").removeClass("current");
-		var url =  $(this).attr("href");
-		var href = url+" #container";
-		$(".container").load(href);
-		var state = {
-			url:url
+		if($(this).attr("class")!==$(".current").attr("class")){
+			var url =  $(this).attr("href");
+			var href = url+" #container";
+			$(".container").load(href);
+			var state = {
+				url:url,
+				subfolder:$(this).attr("class")
+			}
+			window.history.pushState(state,"", url);
+			$(this).addClass("current").parent("li").siblings().children("a").removeClass("current");
 		}
-		window.history.pushState(state,"", url);
-		
 	});
 	$(window).on("popstate",function(){
 		var currentState = history.state.url;
         $(".container").load(currentState+" #container");
+        $(".current").addClass(history.state.subfolder);
 	});
 });
