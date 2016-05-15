1d162ec7b825ddab2e79a36d206a5a53ad559b38
sly <shanglingyue1992@163.com>
2016-05-15 23:10:48 +0800

    task_23

---
+++ IFE/task_23/index.js
@@ -0,0 +1,33 @@
+var list = [];
+var traversal = {
+	bft:function(_root){
+		if (_root!=null) {
+		    list[list.length] = _root;
+			arguments.callee(_root.children[0]);
+		    arguments.callee(_root.children[1]);
+		}
+	}
+}
+
+document.querySelector(".btn_group").onclick = function(e){
+	var fun = null;
+	    list = [];
+	if(e.target.nodeType == 1&&e.target.nodeName.toLowerCase() == 'input'){
+		fun = e.target.getAttribute("id");
+	}
+	(traversal[fun])(document.querySelector(".container"));
+	var i=0;
+    timer = setInterval(function(){
+		if(i<list.length){
+			list[i].style.backgroundColor = "#00F";
+			if(i>0){
+			  list[i-1].style.backgroundColor = "";
+			}
+			i++;
+		}else{
+			list[i-1].style.backgroundColor = "#FFF";
+			clearInterval(timer);
+		}
+   },500)
+
+}
