88cbd3d8b2adba9df88648ef39ac146ed9cb66cf
sly <shanglingyue1992@163.com>
2016-05-15 21:54:39 +0800

    task_22

--- IFE/task_22/index.js
+++ IFE/task_22/index.js
@@ -1,9 +1,8 @@
-var rootNode = document.querySelector(".container");
+var list = [];
 var traversal = {
 	preorder:function(_root){
 		if (_root!=null) {
-			_root.style.backgroundColor = '#00F';
-			_root.style.backgroundColor = '#FFF';
+		    list[list.length] = _root;
 			arguments.callee(_root.children[0]);
 		    arguments.callee(_root.children[1]);
 		}
@@ -11,8 +10,7 @@ var traversal = {
 	inorder:function(_root){
 		if (_root!=null) {
 			arguments.callee(_root.children[0]);
-			_root.style.backgroundColor = '#00F';
-			_root.style.backgroundColor = '#FFF';
+			 list[list.length] = _root;
 		    arguments.callee(_root.children[1]);
 		}
 	},
@@ -20,9 +18,30 @@ var traversal = {
 		if (_root!=null) {
 			arguments.callee(_root.children[0]);
 		    arguments.callee(_root.children[1]);
-		    _root.style.backgroundColor = '#00F';
-			_root.style.backgroundColor = '#FFF';
+		    list[list.length] = _root;
 		}
 	}
 }
-traversal.preorder(rootNode);
+
+document.querySelector(".btn_group").onclick = function(e){
+	var fun = null;
+	    list = [];
+	if(e.target.nodeType == 1&&e.target.nodeName.toLowerCase() == 'input'){
+		fun = e.target.getAttribute("id");
+	}
+	(traversal[fun])(document.querySelector(".container"));
+	var i=0;
+    timer = setInterval(function(){
+		if(i<list.length){
+			list[i].style.backgroundColor = "#00F";
+			if(i>0){
+			  list[i-1].style.backgroundColor = "";
+			}
+			i++;
+		}else{
+			list[i-1].style.backgroundColor = "#FFF";
+			clearInterval(timer);
+		}
+   },500)
+
+}
