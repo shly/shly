feab5872eae3fd6aa3f0988d44bd02bb8cae4b0a
shly <1028442429@qq.com>
2016-05-26 17:27:27 +0800

    index

--- IFE/task_23/index.js
+++ IFE/task_23/index.js
@@ -3,8 +3,9 @@ var traversal = {
 	bft:function(_root){
 		if (_root!=null) {
 		    list[list.length] = _root;
-			arguments.callee(_root.children[0]);
-		    arguments.callee(_root.children[1]);
+		    for (var i = 0,len = _root.children.length; i <len; i++) {
+		    	arguments.callee(_root.children[i]);
+		    }
 		}
 	}
 }
@@ -29,5 +30,20 @@ document.querySelector(".btn_group").onclick = function(e){
 			clearInterval(timer);
 		}
    },500)
-
 }
+document.querySelector("#search").addEventListener('click',function(){
+	 var findContent = document.querySelector("#searchContent").value;
+	  timer = setInterval(function(){
+		if(i<list.length){
+			list[i].style.backgroundColor = "#00F";
+			if(i>0){
+			  list[i-1].style.backgroundColor = "";
+			}
+			i++;
+		}else{
+			list[i-1].style.backgroundColor = "#FFF";
+			clearInterval(timer);
+		}
+   },500)
+
+});
